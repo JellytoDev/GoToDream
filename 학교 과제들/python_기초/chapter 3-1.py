8ad2a2cec09de212a3d169d90f4210e9f624ebf3
HellGateCheonan <wjdwhdgus2222@naver.com>
2021-11-08 23:36:02 +0900

    commit

---
+++ 학교 과제들/python_기초/chapter 3-1.py
@@ -0,0 +1,18 @@
+def Add(a,b):
+    return a+b
+
+def Sub(a,b):
+    return a-b
+
+def Mul(a,b):
+    return a*b
+
+def Div(a,b):
+    return a/b
+
+sum1 =  Mul(20,30) 
+sum2 =  Add(sum1,20)
+
+print("10+20*30 = ",sum2)
+    
+
